36948df5895ff369e9b1b61bb0735198cb5df2f4
Dron Pareek <dronpareek@example.com>
2025-12-19 10:28:39 +0530

    levels add

--- script.js
+++ script.js
@@ -5,17 +5,29 @@ let btn = document.querySelector(".button");
 let info = document.querySelector(".info");
 let main = document.querySelector(".main");
 let instruction = document.querySelector(".instructions");
+let levels = document.querySelector(".levels");
+let easy = document.querySelector(".easy")
+let hard = document.querySelector(".hard")
+let medium = document.querySelector(".medium")
+
 
 let time = 20;
 let interval;
 let score = 0;
+let movingintervall;
+let hardmovingintervall;
+let easymovingintervall;
+let mediumMovingintervall;
+
 
 btn.addEventListener("click", () => {
   redbox.style.display = "flex";
   info.style.display = "flex";
   btn.style.display = "none";
+  levels.style.display = "none"
   instruction.style.display = "none";
   timer();
+  movingredbox();
   timepara.innerText = `Time:${time}`;
 
   scorepara.innerText = `Score : ${score}`;
@@ -33,20 +45,87 @@ function timer() {
     }
   }, 1000);
 }
+function movingredbox() {
+  movingintervall = setInterval(() => {
+    redbox.style.marginLeft = Math.random() * 300 + "px";
+    redbox.style.marginTop = Math.random() * 250 + "px";
+  }, 1300);
+}
 
 redbox.addEventListener("click", () => {
   score++;
   scorepara.innerText = `Score : ${score}`;
-  redbox.style.marginLeft = Math.random() * 300 + "px";
-  redbox.style.marginTop = Math.random() * 250 + "px";
 });
 
 function endgame() {
   redbox.style.display = "none";
   clearInterval(interval);
+  clearInterval(movingintervall);
+  clearInterval(hardmovingintervall)
+  clearInterval(mediumMovingintervall)
+  clearInterval(easymovingintervall)
   info.style.display = "none";
   scorepara.innerHTML = "";
   timepara.innerText = "";
   alert(`Your score is ${score}`);
   btn.style.display = "block";
 }
+
+hard.addEventListener("click", ()=>{
+  hardmovingredbox()
+   redbox.style.display = "flex";
+  info.style.display = "flex";
+  btn.style.display = "none";
+  levels.style.display = "none"
+  instruction.style.display = "none";
+    timer();
+})
+
+function hardmovingredbox() {
+  hardmovingintervall = setInterval(() => {
+    redbox.style.marginLeft = Math.random() * 300 + "px";
+    redbox.style.marginTop = Math.random() * 250 + "px";
+  }, 600);
+}
+
+
+easy.addEventListener("click", ()=>{
+  easymovingredbox()
+   redbox.style.display = "flex";
+  info.style.display = "flex";
+  btn.style.display = "none";
+  levels.style.display = "none"
+  instruction.style.display = "none";
+    timer();
+})
+
+function easymovingredbox() {
+  easymovingintervall = setInterval(() => {
+    redbox.style.marginLeft = Math.random() * 300 + "px";
+    redbox.style.marginTop = Math.random() * 250 + "px";
+  },1300);
+}
+
+medium.addEventListener("click", ()=>{
+  mediumMovingredbox()
+   redbox.style.display = "flex";
+  info.style.display = "flex";
+  btn.style.display = "none";
+  levels.style.display = "none"
+  instruction.style.display = "none";
+    timer();
+})
+
+function mediumMovingredbox() {
+  mediumMovingintervall = setInterval(() => {
+    redbox.style.marginLeft = Math.random() * 300 + "px";
+    redbox.style.marginTop = Math.random() * 250 + "px";
+  }, 900);
+}
+
+
+
+
+
+
+
